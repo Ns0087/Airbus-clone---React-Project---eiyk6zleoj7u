ab9d0feaec2170cfd53c0be13d33492992418a2f
Nitin Saraswat <nitinsaraswat.30@gmail.com>
2023-09-24 20:16:36 +0530

    airbus clone

---
+++ src/app/SearchSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = { from: "", to: "", price: 0, isUserLogin: false };
+
+const SearchSlice = createSlice({
+  name: "search",
+  initialState,
+  reducers: {
+    fromToSetInRedux: (state, action) => {
+      state.from = action.payload.from;
+      state.to = action.payload.to;
+    },
+    flightPrice: (state, action) => {
+      state.price = action.payload;
+    },
+    UserLogin: (state, action) => {
+      state.isUserLogin = action.payload;
+    },
+  },
+});
+
+export const { fromToSetInRedux, flightPrice, UserLogin } = SearchSlice.actions;
+export default SearchSlice.reducer;
